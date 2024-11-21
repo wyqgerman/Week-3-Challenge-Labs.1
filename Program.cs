947a2756577c0929288ed1efe357f56fa86cbc79
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-11-21 16:52:39 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Week_3_Challenge_Labs._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome("home"));
+            Console.WriteLine(IsPalindrome("eye"));
+        }
+
+        static bool IsPalindrome(string input)
+        {
+            int left = 0;
+            int right = input.Length - 1;
+
+            while (left < right)
+            {
+                if (input[left] != input[right])
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+    }
+}
